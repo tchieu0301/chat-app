c739bd627cf6f78ef90808a6985883ae6bd2d00a
Dung Giangg <gtdbmt113@gmail.com>
2023-08-11 10:03:14 +0700

    first commit

---
+++ back-end/ChatApplicationApi/ChatApp.Data/Chat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Data
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+    }
+}
