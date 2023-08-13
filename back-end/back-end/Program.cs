0538eb06f71009f7e0693f183c3eed3e3919846a
Dung Giangg <gtdbmt113@gmail.com>
2023-08-13 10:02:35 +0700

    tailwind

--- back-end/back-end/Program.cs
+++ back-end/back-end/Program.cs
@@ -1,16 +1,26 @@
 using back_end.Contexts;
 using back_end.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ChatApplicationDb>();
+
 var app = builder.Build();
-app.MapPost("/user",CreateAccount);
+var accountEndpoint = app.MapGroup("/account");
+accountEndpoint.MapPost("/", createAccount);
+accountEndpoint.MapGet("/", getAccounts);
 
 app.Run();
 
-static async Task<IResult> CreateAccount(ChatApplicationDb db, Account account)
+static async Task<IResult> createAccount(ChatApplicationDb db, Account account)
 {
     db.Accounts.Add(account);
     await db.SaveChangesAsync();
     return TypedResults.Created($"/user", account);
+}
+
+static async Task<IResult> getAccounts(ChatApplicationDb db)
+{
+    return TypedResults.Ok(await db.Accounts.ToListAsync());
 }
---
+++ front-end/src/pages/test.js
@@ -0,0 +1,8 @@
+
+export default function Tets() {
+  return (
+    <div className="flex justify-center">
+        <h1>Hello World</h1>
+    </div>
+  )
+}
